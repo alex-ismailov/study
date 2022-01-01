afae2371052b732c4534b1ec813ef81dcf6594e1
alex-ismailov <alexx.ismailov@gmail.com>
2022-01-02 00:49:11 +0300

    Add replace and search methods implementations

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,40 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        new_array = []
+        get_max = lambda { |collection|
+          max_num = 0
+          collection.each do |number|
+            max_num = number if number > max_num
+          end
+          max_num
+        }
+
+        max_num = get_max.call(array)
+        array.each do |number|
+          new_array << if number >= 0
+                         max_num
+                       else
+                         number
+                       end
+        end
+
+        new_array
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        iterate = lambda { |collection, first_index, last_index|
+          return -1 if first_index > last_index
+
+          middle_index = first_index + last_index
+          return middle_index if query == collection[middle_index]
+
+          return iterate.call(collection, middle_index + 1, last_index) if query > collection[middle_index]
+
+          iterate.call(collection, first_index, middle_index - 1)
+        }
+
+        iterate.call(array, 0, array.length - 1)
       end
     end
   end
